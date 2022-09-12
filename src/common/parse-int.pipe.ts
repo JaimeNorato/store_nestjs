02d50df6ceba8f420e9da1010bb98393547cb364
Jaime Norato <jn901113jn@gmail.com>
2022-09-12 15:38:44 -0500

    creando pipes propios

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -1,8 +1,12 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+  transform(value: string, metadata: ArgumentMetadata) {
+    const val = parseInt(value, 10);
+    if (isNaN(val)) {
+      throw new BadRequestException(`${value} no es un número`);
+    }
+    return val;
   }
 }
