b0bd3326c4062129e38adf949beb358e574dbc7a
Jaime Norato <jn901113jn@gmail.com>
2022-09-15 15:30:00 -0500

    importando modulos desde otro modulo

---
+++ src/users/services/users.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from '../dtos/users.dtos';
+
+import { User } from './../entities/user.entity';
+import { Order } from './../entities/order.entity';
+
+import { ProductsService } from './../../products/services/products.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private productsService: ProductsService) {}
+  private counterId = 1;
+  private users: User[] = [];
+  /** caraga todos los usuarios */
+  findAll() {
+    return this.users;
+  }
+  /**obtiene un usuario cargado por su id */
+  findOne(id: number) {
+    const user = this.users.find((item) => item.id === id);
+    if (!user) {
+      throw new NotFoundException(`User #${id} not found`);
+    }
+    return user;
+  }
+  /**crea un nuevo usuario */
+  create(payload: CreateUserDto) {
+    this.counterId = this.counterId + 1;
+    const newUser = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+  /**actualiza un usuario */
+  update(id: number, payload: UpdateUserDto) {
+    const user = this.findOne(id);
+    if (user) {
+      const index = this.users.findIndex((item) => item.id === id);
+      this.users[index] = {
+        ...user,
+        ...payload,
+      };
+      return this.users[index];
+    }
+    return null;
+  }
+  /**elimina un usuario */
+  remove(id: number) {
+    const index = this.users.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return new NotFoundException(`User #${id} not found`);
+    }
+    this.users.splice(index, 1);
+    return true;
+  }
+  /**obtiene las ordenes por usuario */
+  getOrdersByUser(id: number): Order {
+    const user = this.findOne(id);
+    return {
+      date: new Date(),
+      user,
+      products: this.productsService.findAll(),
+    };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
 
-@Module({})
+import { ProductsModule } from './../products/products.module';
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
